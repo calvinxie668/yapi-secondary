0e32b2649ba8d4e26452084ebae66d820512875c
calvin xie <963170847@qq.com>
2022-03-30 19:56:27 +0800

    fix: shell脚本代码更改

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,27 @@ echo '执行自动化部署命令开始'
 WORK_DIR='vendors'
 
 cd $1
+echo '安装依赖'
 npm install 
+echo '执行pathc'
 npm run postinstall
+echo '开始打包...'
 ykit pack -m
 
 cd ..
 
 if [ ! -d $(pwd)'/'$WORK_DIR'/' ];then
-echo 'vendors文件夹不存在'
+echo ''$WORK_DIR'文件夹不存在'
 exit 
 else
-echo '清空vendors文件夹'
+echo '清空'$WORK_DIR'文件夹'
 rm -rf $WORK_DIR/*
 fi
 
 echo 'copy '$1'新文件到'$WORK_DIR'文件夹'
-/bin/cp -rf $1/*  $WORK_DIR/*
+/bin/cp -rf $1/*  $WORK_DIR/
 
 echo '重启node服务'
 pm2 delete all 
-pm2 start -i 2 ./$WORK_DIR/server/app.js --name yapi 
+pm2 start -i 1 ./$WORK_DIR/server/app.js --name yapi 
 echo '自动化部署结束'
